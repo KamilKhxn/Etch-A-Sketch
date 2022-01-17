ac9d0f92624475d4157e149db4af1d92f21679c3
Kamil Khan <kamilkhan478@gmail.com>
2022-01-16 22:38:41 -0500

    UPDATED clear button to remove coloured squres

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Etch-A-Sketch application made with JavaScript, CSS and html. Live demo: https://kamilkhxn.github.io/Etch-A-Sketch/
+# Etch A Sketch
+This Etch-A-Sketch application was made with **JavaScript**, **HTML** and **CSS**. Check out a live demo [here](https://kamilkhxn.github.io/Etch-A-Sketch/).
+## About
+The project allows users to colour a grid
+
+## What I learned
+- Github branches (checkout -b "newBranchName")
+- Hover selector in CSS
+- Grids in CSS
+
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const blackBtn = document.querySelector('#black');
 const rainbowBtn = document.querySelector('#rainbow');
 const eraserBtn = document.querySelector('#eraser');
 const clearBtn = document.querySelector('#clear');
-const resizeBtn = document.querySelector('#resize');
+// const resizeBtn = document.querySelector('#resize');
 const grid = document.querySelector('.grid');
 const buttons = document.querySelectorAll("button");
 let currentMode = '';
@@ -19,23 +19,23 @@ function removeButton(button) {
     rainbowBtn.classList.remove('press');
     eraserBtn.classList.remove('press');
     clearBtn.classList.remove('press');
-    resizeBtn.classList.remove('press');
-    
-    if (button === 'black'){
+    // resizeBtn.classList.remove('press');
+
+    if (button === 'black') {
         blackBtn.classList.add('press');
     }
-    if (button === 'rainbow'){
+    if (button === 'rainbow') {
         rainbowBtn.classList.add('press');
     }
-    if (button === 'eraser'){
+    if (button === 'eraser') {
         eraserBtn.classList.add('press');
     }
-    if (button === 'clear'){
+    if (button === 'clear') {
         clearBtn.classList.add('press');
     }
-    if (button === 'resize'){
-        resizeBtn.classList.add('press');
-    }   
+    // if (button === 'resize'){
+    //     resizeBtn.classList.add('press');
+    // }   
 }
 
 function buttonPressed(button) {
@@ -45,12 +45,12 @@ function buttonPressed(button) {
 let gridSize = 16;
 let pixel = '';
 const drawGrid = (screenSize) => {
-    for(i = 0; i < screenSize ** 2; i++){
+    for (i = 0; i < screenSize ** 2; i++) {
         pixel = document.createElement('div');
         pixel.classList.add('squares');//squares
         //pixel.style.backgroundColor = 'white';
         grid.appendChild(pixel);
-    }  
+    }
     grid.style.gridTemplateColumns = `repeat(${screenSize}, auto)`;
     grid.style.gridTemplateRows = `repeat(${screenSize}, auto)`;
 }//end of drawGrid()
@@ -59,18 +59,26 @@ drawGrid(gridSize);
 
 const randomColor = () => {
     let color = 'rgba(';
-    for(let i = 0;i< 3;i++){
-      color += Math.floor(Math.random() * 255) + ',';
+    for (let i = 0; i < 3; i++) {
+        color += Math.floor(Math.random() * 255) + ',';
     }
     return color + '1)';
-  }
+}
 
-const active = () =>{
-    let pixels = document.querySelectorAll('.squares');
-    pixels.forEach(pxl =>{
+const clearGrid = () => {
+    console.log('i was pressed!');
+    pixels.forEach(pxl => {
+        pxl.style.backgroundColor = 'rgb(255,255,255)';
+    })
+    // grid.style.backgroundColor = 'rgb(255,255,255)';
+}
+
+let pixels = document.querySelectorAll('.squares');
+const active = () => {
+    pixels.forEach(pxl => {
         pxl.addEventListener('mouseover', (e) => {
             let crntClr = getComputedStyle(pxl, null).getPropertyValue('background-color');
-            switch (currentMode){
+            switch (currentMode) {
                 case 'black':
                     e.target.style.backgroundColor = 'rgba(0,0,0)';
                     break;
@@ -80,8 +88,11 @@ const active = () =>{
                 case 'eraser':
                     e.target.style.backgroundColor = 'rgb(255,255,255)';
                     break;
+                case 'clear':
+                    clearGrid();
+                    break;
             }
-               })
+        })
     })
 }
 active();
